83f1e74c438a5e6b22ca00b352b105354cf39225
Koulinn <drdverzola@gmail.com>
2021-09-23 13:44:29 +0100

    initial files

---
+++ src/Types/index.ts
@@ -0,0 +1,25 @@
+export interface Title {
+    title: string;
+}
+
+export interface Comments {
+    id: number;
+    rating: number;
+    comment: string;
+    author: string;
+    date: string;
+}
+
+export interface Dish {
+    id: number;
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    description: string;
+    comments:Comments[];
+}
+
+export type SelectedDish = Dish | null
+
